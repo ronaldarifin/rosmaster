722ba5a50337426b883ceef773995f7e359f36e9
Jonathan Goenadibrata <jonathango@berkeley.edu>
2024-11-16 16:33:36 -0800

    add SunriseRobotLib

---
+++ src/trajectory_planner/resource/SunriseRobotLib/__init__.py
@@ -0,0 +1,2 @@
+from .SunriseRobotLib import SunriseRobot
+from .mipi_camera import Mipi_Camera
---
+++ src/trajectory_planner/resource/SunriseRobotLib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SunriseRobotLib',
+    version='2.0.1',
+    author='Yahboom Team',
+    packages=find_packages(),
+)
+
+# cd py_install
+# sudo python3 setup.py install
